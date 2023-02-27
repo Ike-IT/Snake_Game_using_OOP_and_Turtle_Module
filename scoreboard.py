860a69634942464af18d9c0b5f98cc79ac88a593
Cool Breeds <76704122+Ike-IT@users.noreply.github.com>
2023-02-27 08:16:41 +0100

    Initial commit

---
+++ scoreboard.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0   #An attribute was created here
+        with open("data.txt") as data:
+            self.high_score = int(data.read())  #An attribute that reads from file was created here
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}, High score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score  #Here (self.score) will be updated only at the end of each game.
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+
+
